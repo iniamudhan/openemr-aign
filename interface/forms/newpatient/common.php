2c24e7bdacda4c0fecfce77c1ab02926cbca347a
arunkumar <achermadurai@presidio.com>
2023-09-27 19:21:11 +0530

    Waveequilizer try

--- interface/forms/newpatient/common.php
+++ interface/forms/newpatient/common.php
@@ -300,69 +300,71 @@ $ires = sqlStatement("SELECT id, type, title, begdate FROM lists WHERE " .
             }
         }
     </style>
+      <script src="https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.min.js"></script>
+  <script src="https://unpkg.com/wavesurfer.js@7/dist/plugin/wavesurfer.microphone.min.js"></script>
+
     <script>
-
-import WaveSurfer from 'https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js'
-import RecordPlugin from 'https://unpkg.com/wavesurfer.js@7/dist/plugins/record.esm.js'
-
 // Create an instance of WaveSurfer
-const wavesurfer = WaveSurfer.create({
+var wavesurfer = WaveSurfer.create({
   container: '#mic',
   waveColor: 'rgb(200, 0, 200)',
-  progressColor: 'rgb(100, 0, 100)',
-})
+  progressColor: 'rgb(100, 0, 100)'
+});
 
 // Initialize the Record plugin
-const record = wavesurfer.registerPlugin(RecordPlugin.create())
+var record = wavesurfer.registerPlugin(WaveSurfer.plugins.Record.create());
 
 // Render recorded audio
-record.on('record-end', (blob) => {
-  const container = document.querySelector('#recordings')
-  const recordedUrl = URL.createObjectURL(blob)
+record.on('record-end', function (blob) {
+  var container = document.querySelector('#recordings');
+  var recordedUrl = URL.createObjectURL(blob);
 
   // Create wavesurfer from the recorded audio
-  const wavesurfer = WaveSurfer.create({
-    container,
+  var wavesurferRecorded = WaveSurfer.create({
+    container: container,
     waveColor: 'rgb(200, 100, 0)',
     progressColor: 'rgb(100, 50, 0)',
-    url: recordedUrl,
-  })
+    url: recordedUrl
+  });
 
   // Play button
-  const button = container.appendChild(document.createElement('button'))
-  button.textContent = 'Play'
-  button.onclick = () => wavesurfer.playPause()
-  wavesurfer.on('pause', () => (button.textContent = 'Play'))
-  wavesurfer.on('play', () => (button.textContent = 'Pause'))
+  var button = container.appendChild(document.createElement('button'));
+  button.textContent = 'Play';
+  button.onclick = function () {
+    wavesurferRecorded.playPause();
+  };
+  wavesurferRecorded.on('pause', function () {
+    button.textContent = 'Play';
+  });
+  wavesurferRecorded.on('play', function () {
+    button.textContent = 'Pause';
+  });
 
   // Download link
-  const link = container.appendChild(document.createElement('a'))
-  Object.assign(link, {
-    href: recordedUrl,
-    download: 'recording.' + blob.type.split(';')[0].split('/')[1] || 'webm',
-    textContent: 'Download recording',
-  })
-})
+  var link = container.appendChild(document.createElement('a'));
+  link.href = recordedUrl;
+  link.download = 'recording.' + (blob.type.split(';')[0].split('/')[1] || 'webm');
+  link.textContent = 'Download recording';
+});
 
-{
-  // Record button
-  const recButton = document.querySelector('#record')
+// Record button
+var recButton = document.querySelector('#record');
 
-  recButton.onclick = () => {
-    if (record.isRecording()) {
-      record.stopRecording()
-      recButton.textContent = 'Record'
-      return
-    }
-
-    recButton.disabled = true
-
-    record.startRecording().then(() => {
-      recButton.textContent = 'Stop'
-      recButton.disabled = false
-    })
+recButton.onclick = function () {
+  if (record.isRecording()) {
+    record.stopRecording();
+    recButton.textContent = 'Record';
+    return;
   }
-}
+
+  recButton.disabled = true;
+
+  record.startRecording().then(function () {
+    recButton.textContent = 'Stop';
+    recButton.disabled = false;
+  });
+};
+
         </script>
     <?php
     if ($viewmode) {
