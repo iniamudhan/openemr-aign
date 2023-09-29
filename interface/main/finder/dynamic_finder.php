c0fcc692d12c8cf19266a8024e701695df01264f
Iniamudhan Baskar <ibaskar@presidio.com>
2023-09-29 15:33:55 +0530

    Update dynamic_finder.php

--- interface/main/finder/dynamic_finder.php
+++ interface/main/finder/dynamic_finder.php
@@ -373,7 +373,9 @@ $loading = "<div class='spinner-border' role='status'><span class='sr-only'>" . 
                 <button id="create_patient_btn1" class="btn btn-primary btn-add" onclick="top.restoreSession();top.RTop.location = '<?php echo $web_root ?>/interface/new/new.php'"><?php echo xlt('Add New Patient'); ?></button>
             <?php } ?>
             <div>
-                <input type="text" />
+            <div class="dataTables_filter d-md-initial" style="float: left;">
+                <label>Search:<input type="AI search" class="form-control form-control-sm" placeholder="" id="ai_search" aria-controls="pt_table"></label>
+            </div>
                 <div id="dynamic"><!-- TBD: id seems unused, is this div required? -->
                     <!-- Class "display" is defined in demo_table.css -->
                     <div class="table-responsive">
