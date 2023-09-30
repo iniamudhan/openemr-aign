7fa0503f017a2fc20471a77db7c3b15d23575edb
Iniamudhan Baskar <ibaskar@presidio.com>
2023-09-30 07:49:41 +0530

    Update save.php

--- interface/forms/newpatient/save.php
+++ interface/forms/newpatient/save.php
@@ -232,6 +232,47 @@ if ($mode == 'new') {
     die("Unknown mode '" . text($mode) . "'");
 }
 
+// URL of the API endpoint
+$url = 'http://54.224.124.129:5000';
+
+// Data you want to send in JSON format
+$data = array(
+    'patientId' => $pid,
+    'summary' => $ai_summary,
+    'diagnosis' => $ai_diagnosis
+);
+$json_data = json_encode($data);
+
+// Initialize cURL session
+$ch = curl_init($url);
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     // Return response instead of printing
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");    // Specify the request method as POST
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);   // Set the JSON data as the POST data
+
+// Setting headers for JSON content type
+$headers = array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($json_data),
+    // ... you can add more headers if required
+);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+// Execute cURL session and get the response
+$response = curl_exec($ch);
+
+// Check for cURL errors
+if ($error = curl_error($ch)) {
+    die('Error making API call: ' . $error);
+}
+
+// Close cURL session
+curl_close($ch);
+
+// Print the API response
+echo $response;
+
 setencounter($encounter);
 
 // Update the list of issues associated with this encounter.
