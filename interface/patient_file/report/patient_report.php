86e070029d700aa99460c987cd88305a47a482ca
Iniamudhan Baskar <ibaskar@presidio.com>
2023-09-30 08:43:32 +0530

    Update patient_report.php

--- interface/patient_file/report/patient_report.php
+++ interface/patient_file/report/patient_report.php
@@ -581,7 +581,7 @@ $oemr_ui = new OemrUI($arrOeUiSettings);
                 },
                 success: function (data, status, jqXHR) {
                     console.log(data);
-                    $("#ai_d_summary").val(data.body.htmlValue)
+                    $("#ai_d_summary").html(data.body.htmlValue)
                 },
 
                 error: function (jqXHR, status) {
