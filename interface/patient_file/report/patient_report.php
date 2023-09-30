30526785e2d4e7219c6626cc8251b0a04b0307d3
Iniamudhan Baskar <ibaskar@presidio.com>
2023-09-30 08:48:51 +0530

    Update patient_report.php

--- interface/patient_file/report/patient_report.php
+++ interface/patient_file/report/patient_report.php
@@ -568,7 +568,7 @@ $oemr_ui = new OemrUI($arrOeUiSettings);
             $.ajax({
                 type: "POST",
                 url: "http://44.200.53.13:5000",
-                data: JSON.stringify({"patientId": "1"}),// now data come in this function
+                data: JSON.stringify({"patientId": <?php $pid; ?>}),// now data come in this function
                 contentType: "application/json;",
                 crossDomain: true,
                 dataType: "json",
