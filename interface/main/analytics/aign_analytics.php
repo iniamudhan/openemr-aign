5e260ffb27bad3bd9fe3c06b84af35d2524073de
arunkumar <achermadurai@presidio.com>
2023-10-02 18:17:22 +0530

    try: page creation

---
+++ interface/main/analytics/aign_analytics.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * aign_analytics.php
+ *
+ * Sponsored by David Eschelbacher, MD
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Rod Roark <rod@sunsetsystems.com>
+ * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @author    Jerry Padgett <sjpadgett@gmail.com>
+ * @copyright Copyright (c) 2012-2016 Rod Roark <rod@sunsetsystems.com>
+ * @copyright Copyright (c) 2018 Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2019 Jerry Padgett <sjpadgett@gmail.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+require_once(dirname(__FILE__) . "/../../globals.php");
+require_once "$srcdir/user.inc.php";
+require_once "$srcdir/options.inc.php";
+
+use OpenEMR\Common\Acl\AclMain;
+use OpenEMR\Common\Csrf\CsrfUtils;
+use OpenEMR\Core\Header;
+use OpenEMR\OeUI\OemrUI;
+
+// Generate some code based on the list of columns.
+//
+
+$loading = "<div class='spinner-border' role='status'><span class='sr-only'>" . xlt("Loading") . "...</span></div>";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php Header::setupHeader(['datatables', 'datatables-colreorder', 'datatables-dt', 'datatables-bs']); ?>
+    <title><?php echo xlt("Patient Finder"); ?></title>
+    <link rel="stylesheet" href="<?php echo $webroot; ?>/public/themes/aign_style.css?v=<?php echo $v_js_includes; ?>">
+
+
+<?php
+    $arrOeUiSettings = array(
+    'heading_title' => xl('AIGn Analytics'),
+    'include_patient_name' => false,
+    'expandable' => true,
+    'action' => "search",//conceal, reveal, search, reset, link or back
+    'action_title' => "",//only for action link, leave empty for conceal, reveal, search
+    'action_href' => "",//only for actions - reset, link or back
+    'show_help_icon' => false,
+    'help_file_name' => ""
+    );
+    $oemr_ui = new OemrUI($arrOeUiSettings);
+    ?>
+</head>
+<body>
+    <div id="container_div" class="<?php echo attr($oemr_ui->oeContainer()); ?> mt-3">
+         <div class="w-100">
+            <?php echo $oemr_ui->pageHeading() . "\r\n"; ?>
+            
+           
+    </div> <!--End of Container div-->
+    <?php $oemr_ui->oeBelowContainerDiv();?>
+</body>
+</html>
